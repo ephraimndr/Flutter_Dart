143dbeb14ea76fd94c0474caa6a90184415be21c
Ephraim <ephraimndoromuinde@gmail.com>
2024-12-07 12:53:04 +0300

    Basic Dart Programs

---
+++ day 02/main.dart
@@ -0,0 +1,10 @@
+// Define a function
+printNumber(num number){
+  print("The number is $number");
+}
+
+//This is where the App starts executing
+main(){
+  var number = 42;
+  printNumber(number);
+}
